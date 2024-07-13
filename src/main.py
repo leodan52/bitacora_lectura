702cbd2c19d8cd5aa9b0a108073bffc8826c208e
Leonardo D. Santiago <leodan.52@gmail.com>
2024-07-13 12:51:11 -0600

    Mejorar sintaxis del archivo

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 from controllers.madre_controller import VentanaMadre
 from PyQt5.QtWidgets import QApplication
 
-
-def main():
+if __name__ == "__main__":
 	app = QApplication([])
 	Ventana = VentanaMadre()
 	Ventana.show()
 	app.exec_()
-
-
-main()
